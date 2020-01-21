9646e97b832268873c7425a063e6281b73375752
Amina8989 <55687384+Amina8989@users.noreply.github.com>
2020-01-21 21:11:13 +0100

    odded

---
+++ phpopgave.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Side 1</title>
+        <link rel="stylesheet" href="eksempel.css">
+    </head>
+    <body>
+        <header>
+            <span id="headertekst">Sidens hoved</span>
+        </header>
